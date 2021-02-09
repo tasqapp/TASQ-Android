ee8154fb357f7463d34e377a74bfb01e35f45974
kipnisd <kipnisd@wwu.edu>
2021-02-08 17:00:38 -0800

    Touch ups to some visual elements

--- app/src/main/java/tasq/app/ui/monthly/MonthlyFragment.java
+++ app/src/main/java/tasq/app/ui/monthly/MonthlyFragment.java
@@ -16,7 +16,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
+//import androidx.lifecycle.ViewModelProviders;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
