7d187806ca25b5596c4f882bce8b0796bd1347ca
kipnisd <kipnisd@wwu.edu>
2021-02-12 17:28:48 -0800

    Added date to daily screen

--- app/src/main/java/tasq/app/ui/daily/DailyFragment.java
+++ app/src/main/java/tasq/app/ui/daily/DailyFragment.java
@@ -1,29 +1,30 @@
 package tasq.app.ui.daily;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.text.SimpleDateFormat;
+import java.util.Date ;
+import java.util.Locale;
 
 import tasq.app.R;
-import tasq.app.Task;
 import tasq.app.ui.addedit.AddEditViewModel;
 
 public class DailyFragment extends Fragment {
 
     private DailyViewModel mViewModel;
     private AddEditViewModel model;
+
+    private TextView dailyDate ;
 
     public static DailyFragment newInstance() {
         return new DailyFragment();
@@ -40,9 +41,14 @@ public class DailyFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(getActivity()).get(DailyViewModel.class);
         model = new ViewModelProvider(requireActivity()).get(AddEditViewModel.class);
+
         model.getTask().observe(getViewLifecycleOwner(), item -> {
             Log.d("DAILY", "Got event");
         });
+
+        dailyDate = getActivity().findViewById(R.id.daily_screen_date) ;
+        String curDate = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date()) ;
+        dailyDate.setText(curDate);
     }
 
     //add buttons to the scrollView
--- app/src/main/java/tasq/app/ui/daily/DailyViewModel.java
+++ app/src/main/java/tasq/app/ui/daily/DailyViewModel.java
@@ -3,5 +3,5 @@ package tasq.app.ui.daily;
 import androidx.lifecycle.ViewModel;
 
 public class DailyViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
 }
