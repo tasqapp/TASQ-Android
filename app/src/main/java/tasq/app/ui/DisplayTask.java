8e1b1a1c705f66ad4e3deebd57fbb6677b4803b4
hannahbuzard1 <buzardh@wwu.edu>
2021-02-12 20:12:32 -0800

    -added display task fragment

---
+++ app/src/main/java/tasq/app/ui/DisplayTask.java
@@ -0,0 +1,38 @@
+package tasq.app.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tasq.app.R;
+
+public class DisplayTask extends Fragment {
+
+    private DisplayTaskViewModel mViewModel;
+
+    public static DisplayTask newInstance() {
+        return new DisplayTask();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.display_task_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DisplayTaskViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/tasq/app/ui/DisplayTaskViewModel.java
@@ -0,0 +1,7 @@
+package tasq.app.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class DisplayTaskViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
