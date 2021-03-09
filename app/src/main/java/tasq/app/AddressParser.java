2809a7423bc2a62190ff0923c2d70f8bb34e8a5d
Tyler Kjeldgaard <tyk911@gmail.com>
2021-03-09 10:48:37 -0800

    Add address parser. Add address parsing to add/edit screen

---
+++ app/src/main/java/tasq/app/AddressParser.java
@@ -0,0 +1,27 @@
+package tasq.app;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AddressParser {
+    public Address parseAddress(Context context, String address) {
+        Geocoder geocoder = new Geocoder(context);
+        try {
+            // Try to get an address from the address given.
+            List<Address> addressList = geocoder.getFromLocationName(address, 3);
+            if (!addressList.isEmpty()) {
+                return addressList.get(0);
+            }
+
+        } catch (IOException e) {
+            // No network connection
+            return null;
+        }
+
+        return null;
+    }
+}
